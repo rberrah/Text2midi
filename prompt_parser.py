6c20da863070523128b208c2a0898e6d8f1cc16a
rberrah <racym.berrah@hotmail.com>
2025-04-21 20:21:37 +0200

    Create prompt_parser.py

---
+++ prompt_parser.py
@@ -0,0 +1,9 @@
+def parse_prompt(prompt):
+    # Ici tu peux utiliser des regex ou un modèle NLP plus avancé
+    # Extrait le style, les instruments, le tempo, l'ambiance...
+    return {
+        "style": "pop",
+        "tempo": 130,
+        "mood": "joyful",
+        "instruments": ["piano", "bass"]
+    }
